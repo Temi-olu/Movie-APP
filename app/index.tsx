503251faa0fe57b15d17780d50e7fd484d4bd3fa
Temi-olu <temiloluwaolumodan@gmail.com>
2025-09-20 14:29:27 +0100

    SetUp of the Movie-App

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Indenpx() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-primary font-bold text-2xl">Welcome!!</Text>
+    </View>
+  );
+}
